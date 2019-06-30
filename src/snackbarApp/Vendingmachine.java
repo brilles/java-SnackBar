b022dea2f7f393ff3efb8687e962172184e3f274
Brian Illes <brianrilles@gmail.com>
2019-06-30 08:09:26 -0700

    constructs Vendingmachine class

---
+++ src/snackbarApp/Vendingmachine.java
@@ -0,0 +1,39 @@
+package snackbarApp;
+
+public class Vendingmachine
+{
+	private static int maxId = 0;
+	public int id;
+	public String name;
+
+	public Snack(String name)
+	{
+		this.id = maxId++;
+		this.name = name;
+	}
+
+	// set id
+	public void setId(int id) 
+	{
+		this.id = id;
+	}
+
+	// get id
+	public int getId() 
+	{
+		return id;
+	}
+
+	// set name
+	public void setName(String name)
+	{
+		this.name = name
+	} 
+
+	// get name
+	public String getName()
+	{
+		return name
+	}
+
+}
